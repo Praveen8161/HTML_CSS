edc1e7022aa59cfab50a47f54632ee8aa0e2c64e
Praveen8161 <rbalu8161@gmail.com>
2023-11-03 14:21:33 +0530

    branch created

--- Asycn/first/script.js
+++ Asycn/first/script.js
@@ -1,5 +1,5 @@
 // JS is the only language that can manipulate DOM 
 //JS is the only language supported by Browser
 //Front-end is non-sequential 
-//back-end is sequentiak
+//back-end is sequential
 //JS is a single thread programe
